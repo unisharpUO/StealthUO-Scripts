293234a01b1df96f2edb7e7522367c4a4be79765
unisharpUO <61708311+unisharpUO@users.noreply.github.com>
2020-10-21 13:27:57 -0400

    updated

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
+import json
 from py_stealth import *
 from helpers import *
-from lib.weapon import weapon
+from lib.weapon import *
+from lib.jewelry import *
 
 
 UseHeal = True
@@ -9,5 +11,17 @@ Bandaging = False
 
 if __name__ == '__main__':
     _target = RequestTarget()
-    _weapon = weapon(_target)
-    print(f'damage type: {_weapon.ElementalDamage}')
+    UseObject(_target)
+    Wait(250)
+    _startTime = time.time()
+    _foundList = []
+    _weapons = []
+    _jewelry = []
+    if FindTypesArrayEx([0xFFFF], [0xFFFF], [_target], True):
+        _foundList = GetFindedList()
+        for _found in _foundList:
+            _jewelry.append(Jewelry(_found))
+    _endTime = time.time()
+    print(f'{len(_jewelry)} weapons parsed in {_endTime - _startTime}')
+    for _jewel in _jewelry:
+        print(f'{json.dumps(_jewel.Encoder())}, ')
