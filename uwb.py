cfef2b4315c5f0d817964705ba5b06e1c9fe617f
unisharpUO <>
2024-07-18 09:45:25 -0400

    - added skill checks - changed attack - added healing methods

--- uwb.py
+++ uwb.py
@@ -4,7 +4,6 @@ from lib.helpers import *
 LeaderID = 37928828
 AutoFollow = False
 Confidence = False
-BossType = [704]
 
 
 def onPartyInvite(senderID):
@@ -14,7 +13,6 @@ def onPartyInvite(senderID):
 
 
 def onClilocSpeech(senderID, senderName, clilocID, message):
-    print(message)
     global Confidence
     if 'exude' in message:
         Confidence = True
@@ -46,29 +44,53 @@ if __name__ == '__main__':
     SetMoveThroughCorner(1)
     SetMoveBetweenTwoCorners(1)
     SetMoveThroughNPC(1)
+    SpiritSpeak = False
+    Fencing = False
+    Bushido = False
+    Chivalry = False
+    BossType = 704
+    # Nosfentor |Shadowlord of Cowardice
+
+    if GetSkillCurrentValue('Spirit Speak') > 90:
+        SpiritSpeak = True
+
+    if GetSkillCurrentValue('Fencing') > 90:
+        Fencing = True
+
+    if GetSkillCurrentValue('Bushido') > 90:
+        Bushido = True
+
+    if GetSkillCurrentValue('Chivalry') > 60:
+        Chivalry = True
 
     while True:
-        Wait(500)
+        Wait(250)
 
         if AutoFollow:
             if (GetX(Self()) != GetX(LeaderID)) or (GetY(Self()) != GetY(LeaderID)):
                 MoveXY(GetX(LeaderID), GetY(LeaderID), False, 0, True)
 
         if GetHP(Self()) < (GetMaxHP(Self()) - 20):
-            if not Confidence:
+            if Bushido and not Confidence:
                 Cast('Confidence')
                 Wait(250)
-            if GetActiveAbility() == "0":
+            if GetActiveAbility() == "0" and Fencing:
                 UsePrimaryAbility()
+                Wait(250)
+            if SpiritSpeak:
+                UseSkill('Spirit Speak')
+                Wait(250)
+            if Chivalry:
+                CastToObj('Close Wounds', Self())
+                Wait(250)
+            Wait(250)
 
-        if FindType(146, Ground()):
-            ShadowLord = FindItem()
-            if not AutoFollow:
+        if not AutoFollow:
+            if FindType(BossType, Ground()):
+                ShadowLord = FindItem()
                 if GetX(Self()) != GetX(ShadowLord) or GetY(Self()) != GetY(ShadowLord):
                     MoveXY(GetX(ShadowLord), GetY(ShadowLord), False, 0, True)
                 Attack(ShadowLord)
-        else:
-            ShadowLord = 0
 
-        if GetActiveAbility() == "0":
+        if not AutoFollow and GetActiveAbility() == "0":
             UseSecondaryAbility()
